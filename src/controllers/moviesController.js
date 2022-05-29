c1e675f601ab5bd1e466697d750f0b527720b30a
HawkDiego <giraldito99@gmail.com>
2022-05-29 01:21:12 -0500

    add to controler new method (detail)

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -2,11 +2,22 @@ const db = require("../database/models");
 
 const controller = {
   list: (req, res) => {
-    db.Movie.findAll().then((movies) => {
-      return res.render("moviesList", { movies });
-    }).catch(e => {
+    db.Movie.findAll()
+      .then((movies) => {
+        return res.render("moviesList", { movies });
+      })
+      .catch((e) => {
         console.log(e);
-    });
+      });
+  },
+  detail: (req, res) => {
+    db.Movie.findByPk(req.params.id)
+      .then((movie) => {
+        return res.render("moviesDetail", { movie });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   },
 };
 
